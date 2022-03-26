f871575661e4b4b743dc4a7a11c7cdf60fd286ea
Braiant Cuevas <braiant.cuevas@hotmail.com>
2022-03-26 11:19:05 -0600

    Primer Commit

---
+++ Selenium/src/main/java/test/Selenium.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String exePath = "./driver/chromedriver.exe"; // especificar la ruta del ChomeDriver
+		System.setProperty("webdriver.chrome.driver", exePath); //Agregando las propiedades con la ruta del chrome driver
+		WebDriver driver = new ChromeDriver(); //Objeto de webDriver (cuando faltan librerias el IDE las puede agregar automaticamente)
+		driver.get("https://www.google.com");
+	}
+
+}
---
+++ Selenium/src/test/java/test/TestSel.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestSel {
+	@Test
+	public void selenium_test01() {
+		String exePath = "./driver/chromedriver.exe"; // especificar la ruta del ChomeDriver
+		System.setProperty("webdriver.chrome.driver", exePath); // Agregando las propiedades con la ruta del chrome
+																// driver
+		WebDriver driver = new ChromeDriver(); // Objeto de webDriver (cuando faltan librerias el IDE las puede agregar
+												// automaticamente)
+		driver.get("https://www.google.com");
+	}
+}
