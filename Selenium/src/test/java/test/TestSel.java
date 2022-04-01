6a2f8e005c7873b99072a5df1fd4f34c3e8eac7a
braia <braia@DESKTOP-63IQPQG>
2022-03-31 19:51:11 -0600

    adding go to youtube

--- Selenium/src/test/java/test/TestSel.java
+++ Selenium/src/test/java/test/TestSel.java
@@ -13,14 +13,6 @@ public class TestSel {
 		WebDriver driver = new ChromeDriver(); // Objeto de webDriver (cuando faltan librerias el IDE las puede agregar
 												// automaticamente)
 		driver.get("https://www.google.com");
-	}
-
-	public void selenium_test02() {
-		String exePath = "./driver/chromedriver.exe"; // especificar la ruta del ChomeDriver
-		System.setProperty("webdriver.chrome.driver", exePath); // Agregando las propiedades con la ruta del chrome
-																// driver
-		WebDriver driver = new ChromeDriver(); // Objeto de webDriver (cuando faltan librerias el IDE las puede agregar
-												// automaticamente)
-		driver.get("https://mail.google.com/");
+		driver.get("https://www.youtube.com/");
 	}
 }
